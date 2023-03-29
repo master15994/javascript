c59a47daac5f0a2f699201427cba1556ccd0a0d4
Askar Pulatov <master15994@gmail.com>
2023-03-29 20:33:32 +0300

    class

--- repeat.ts
+++ repeat.ts
@@ -1,41 +1,104 @@
-interface ICar {
-  brand: string;
-  color: string;
-  year: number;
-}
+const news = [
+  {
+    title: 'Я сегодня поел',
+    date: 'Сегодня',
+    price: 1001,
+    subtitle: 'Курицу',
+    content: 'Сегодня был веселый день...',
+    images: ['feqfqeeq', 'feqfeqfqe', '12414', 'feqfqeofq'],
+    links: ['https://koklp', 'https://12312'],
+    partners: [
+      {
+        title: 'Мы за все хорошее, ',
+        site: 'https://nfjefq',
+        keywords: ['Мы хорошие', 'Мы красивые'],
+      },
 
-class Car implements ICar {
-  brand!: string;
-  color!: string;
-  year!: number;
-  isNew!: boolean;
+      {
+        title: 'Мы за все хорошее2, ',
+        site: 'https://nfjefq2',
+        keywords: ['Мы хорошие2', 'Мы красивые2'],
+      },
+    ],
+  },
 
-  constructor(data: ICar) {
-    Object.assign(this, data);
+  {
+    title: 'Я сегодня поел3',
+    subtitle: 'Курицу3',
+    content: 'Сегодня был веселый день...3',
+    images: ['https://fueqfuqehfeq', 'https://jfiq'],
+    links: ['https://koklp', 'https://12312'],
+    partners: [
+      {
+        title: 'Мы за все хорошее, ',
+        site: 'https://nfjefq',
+        keywords: ['Мы хорошие', 'Мы красивые'],
+      },
+      {
+        title: 'Мы за все хорошее2, ',
+        site: 'https://nfjefq2',
+        keywords: ['Мы хорошие2', 'Мы красивые2'],
+      },
+    ],
+  },
+];
+if (!news || news.length === 0) {
+  console.log('Новостей нет');
+} else {
+  const firstNew = news[0];
+  const secondNew = news[1];
+
+  if (firstNew.images.length !== 0) {
+    console.log(firstNew.images[0]);
+  }else (firstNew.images.length === 0){
+    console.log("картинки няма ");
+  }
+
+  if ('title ' in firstNew && 'subtitle' in firstNew) {
+  }
+  if ('date' in firstNew) {
+    console.log('Опубликовать');
   }
 }
 
-const cars: ICar[] = [
-  {
-    brand: 'Tesla',
-    color: 'red',
-    year: 1945,
-  },
-  {
-    brand: 'Honda',
-    color: 'black',
-    year: 1955,
-  },
-  {
-    brand: 'Kia',
-    color: 'purple',
-    year: 1933,
-  },
-];
+// interface ICar {
+//   brand: string;
+//   color: string;
+//   year: number;
+// }
 
-const carsInstanses = cars.map(item => new Car(item));
+// class Car implements ICar {
+//   brand!: string;
+//   color!: string;
+//   year!: number;
+//   isNew!: boolean;
 
-console.log(carsInstanses);
+//   constructor(data: ICar) {
+//     Object.assign(this, data);
+//   }
+// }
+
+// const cars: ICar[] = [
+//   {
+//     brand: 'Tesla',
+//     color: 'red',
+//     year: 1945,
+//   },
+//   {
+//     brand: 'Honda',
+//     color: 'black',
+//     year: 1955,
+//   },
+//   {
+//     brand: 'Kia',
+//     color: 'purple',
+//     year: 1933,
+//   },
+// ];
+
+// const carsInstanses = cars.map(item => new Car(item));
+
+// console.log(carsInstanses);
 
 // const car = cars.filter(item => item.color === 'red');
 // console.log(car);
